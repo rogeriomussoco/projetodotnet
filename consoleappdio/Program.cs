d356ece61b48641756464da09692bd3122415a59
rogeriomussoco <rogeriotiagomiguelit@gmail.com>
2021-12-25 22:53:39 -0300

    continuar cod do console

--- consoleappdio/Program.cs
+++ consoleappdio/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace consoleappdio // Note: actual namespace depends on the project name.
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int numeroDeVezes = 10;
+            for (int i = 0; i < numeroDeVezes; i++)
+            {
+                Console.WriteLine($"Bem vindo ao Curso de DotNet {i}");
+            }
+
+        }
+    }
+}
