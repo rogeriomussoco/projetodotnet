033e9cbcafdd8733ba74df3b5d1b0f08dc3cd00d
rogeriomussoco <rogeriotiagomiguelit@gmail.com>
2021-12-28 21:17:15 -0300

    continuando com o projeto

--- apprevisao/Program.cs
+++ apprevisao/Program.cs
@@ -1,2 +1,49 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace Apprevisao
+{
+    class Program 
+        {
+        static void Main(string[] args)
+        {
+            string opcaoUsuario = ObterOpcaoUsuario();
+
+            while (opcaoUsuario.ToUpper() != "x")
+            {
+                switch (opcaoUsuario)
+                {
+                    case "1":
+                        //TODO:adicionar aluno
+                        break;
+
+                    case "2":
+                        //TODO:Listar alunos
+                        break;
+
+                    case "3":
+                        //TODO: calcular media
+                        break;
+
+                    default:
+
+                        throw new ArgumentOutOfRangeException();
+                }
+                opcaoUsuario = ObterOpcaoUsuario();
+            }
+
+        }
+
+        private static string ObterOpcaoUsuario()
+        {
+            Console.WriteLine("informe a opcao desejada");
+            Console.WriteLine("1:inserir novo aluno");
+            Console.WriteLine("2: Calcular a média artimética geral");
+            Console.WriteLine("X:Sair:");
+            Console.WriteLine();
+
+            string opcaoUsuario = Console.ReadLine();
+            return opcaoUsuario;
+        }
+    }
+
+}
